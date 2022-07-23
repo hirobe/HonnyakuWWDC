67e281d14f8230d25ea2833255ab9f9fe161111c
Kazuya Hirobe <k.hiorbe@gmail.com>
2022-07-23 09:36:58 +0900

    Lint

--- HonnyakuWWDCClassicPlayer/ClassicPlayerSettingView.swift
+++ HonnyakuWWDCClassicPlayer/ClassicPlayerSettingView.swift
@@ -15,106 +15,85 @@ struct ClassicPlayerSettingView: View {
     var action: ((_ action: Action) throws -> Void)?
 
     var body: some View {
-        //VStack(spacing: 0) {
-            /*
-            HStack {
-                Spacer()
-                Button(action: {
-                    withAnimation {
-                        showingPopup = false
-                    }
-                }) {
-                    Image(systemName: "xmark")
-                        .font(.system(size: 22))
-                        //                        .foregroundColor(.gray)
-                        .padding(EdgeInsets(top: 20, leading: 10, bottom: 16, trailing: 6))
+        Form {
+            Section {
+                HStack {
+                    Text("Speech Volume")
+                    Slider(value: $viewModel.speechVolume)
                 }
-                .buttonStyle(PlainButtonStyle())
-                .frame(width: 44, height: 56, alignment: .center)
-                .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 15))
+                HStack {
+                    Text("Video Volume")
+
+                    Slider(value: $viewModel.videoVolume)
+                }
+                HStack {
+                    Text("音声")
+                }
+                Toggle("オリジナルテキストを表示", isOn: $viewModel.showOriginalText)
+                Toggle("翻訳テキストを表示", isOn: $viewModel.showTransferdText)
+            } header: {
+                Text("再生")
             }
-            .background(Color(.systemGroupedBackground))
-*/
-            Form {
-                Section {
-                    HStack {
-                        Text("Speech Volume")
-                        Slider(value: $viewModel.speechVolume)
-                    }
-                    HStack {
-                        Text("Video Volume")
 
-                        Slider(value: $viewModel.videoVolume)
-                    }
-                    HStack {
-                        Text("音声")
-                    }
-                    Toggle("オリジナルテキストを表示", isOn: $viewModel.showOriginalText)
-                    Toggle("翻訳テキストを表示", isOn: $viewModel.showTransferdText)
-                } header: {
-                    Text("再生")
-                }
+            Section {
+                HStack {
+                    Text("言語")
+                    Spacer()
+                    Picker("", selection: $viewModel.selectedLanguageId) {
+                        ForEach(viewModel.languages()) { language in
+                            Text("\(language.name)")
+                                .tag(language.id)
 
-                Section {
-                    HStack {
-                        Text("言語")
-                        Spacer()
-                        Picker("", selection: $viewModel.selectedLanguageId) {
-                            ForEach(viewModel.languages()) { language in
-                                Text("\(language.name)")
-                                    .tag(language.id)
-
-                            }
-                        }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    HStack {
-                        Text("音声")
-                        Spacer()
-                        let list = viewModel.voices(languageId: viewModel.selectedLanguageId)
-                        if list.count > 0 {
-                            Picker("", selection: $viewModel.selectedVoiceId) {
-                                ForEach(list) { voice in
-                                    Text("\(voice.title)")
-                                        .tag(voice.id)
-                                }
-                            }
-                            .pickerStyle(MenuPickerStyle())
-                        } else {
-                            Text("Not found")
                         }
                     }
                 }
-
-                Section {
-                    Button("データを貼り付ける") {
-                        Task {
-                            guard (try? action?(.pasteData)) != nil else {
-                                isShowingAlertForLoad = true
-                                return
-                            }
-                            withAnimation {
-                                showingPopup = false
-                            }
-                        }
-                    }
-                    /*
-                    Button("データをLoad") {
-                        Task {
-                            guard (try? action?(.loadData)) != nil else {
-                                isShowingAlertForLoad = true
-                                return
-                            }
-                            withAnimation {
-                                showingPopup = false
+                .pickerStyle(MenuPickerStyle())
+                HStack {
+                    Text("音声")
+                    Spacer()
+                    let list = viewModel.voices(languageId: viewModel.selectedLanguageId)
+                    if list.count > 0 {
+                        Picker("", selection: $viewModel.selectedVoiceId) {
+                            ForEach(list) { voice in
+                                Text("\(voice.title)")
+                                    .tag(voice.id)
                             }
                         }
+                        .pickerStyle(MenuPickerStyle())
+                    } else {
+                        Text("Not found")
                     }
-                    .alert(isPresented: $isShowingAlertForLoad) {
-                        Alert(title: Text("Failed"))
-                    }*/
                 }
-            //}
+            }
+
+            Section {
+                Button("データを貼り付ける") {
+                    Task {
+                        guard (try? action?(.pasteData)) != nil else {
+                            isShowingAlertForLoad = true
+                            return
+                        }
+                        withAnimation {
+                            showingPopup = false
+                        }
+                    }
+                }
+                /*
+                Button("データをLoad") {
+                    Task {
+                        guard (try? action?(.loadData)) != nil else {
+                            isShowingAlertForLoad = true
+                            return
+                        }
+                        withAnimation {
+                            showingPopup = false
+                        }
+                    }
+                }
+                .alert(isPresented: $isShowingAlertForLoad) {
+                    Alert(title: Text("Failed"))
+                }*/
+            }
             .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
         }
     }
