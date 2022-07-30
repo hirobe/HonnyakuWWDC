0320a1bf1371c18b394676d3eefdb88ebea29568
Kazuya Hirobe <k.hiorbe@gmail.com>
2022-07-30 10:44:38 +0900

    Fix video rate bug

--- HonnyakuWWDC/VideoPlayer/ViewModel/PlayerViewModel.swift
+++ HonnyakuWWDC/VideoPlayer/ViewModel/PlayerViewModel.swift
@@ -79,7 +79,9 @@ final class PlayerViewModel: ObservableObject {
         }
         .store(in: &cancellables)
         settingsUseCase.$videoRate.sink { [weak self] value in
-            self?.videoPlayer.rate = Float(value) // rateはplayの直後に再設定する必要がある
+            guard let self = self,
+                  self.videoPlayer.rate > 0 else { return } // 停止中はrateを変えない
+            self.videoPlayer.rate = Float(value) // rateはplayの直後に再設定する必要がある
         }
         .store(in: &cancellables)
         settingsUseCase.$showOriginalText.sink { [weak self] value in
